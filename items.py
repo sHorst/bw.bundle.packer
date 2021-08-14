3df0b93065cf05f86f1b24bbc03f66febcdade6a
Stefan Horst <stefan@ultrachaos.de>
2021-08-14 19:24:06 +0200

    move to apt repo

--- items.py
+++ items.py
@@ -1,23 +1,3 @@
-PACKER_VER = '1.4.2'
-PACKER_SUM = '2fcbd1662ac76dc4dec381bdc7b5e6316d5b9d48e0774a32fe6ef9ec19f47213'
-
-
-directories = {
-    '/opt/packer': {
-        'owner': 'root',
-        'group': 'root',
-        'mode': "0751",
-    },
-}
-
-downloads = {
-    '/opt/packer/packer_{0}.zip'.format(PACKER_VER): {
-        'url': 'https://releases.hashicorp.com/packer/{0}/packer_{0}_linux_amd64.zip'.format(PACKER_VER),
-        'sha256': PACKER_SUM,
-        'needs': ['directory:/opt/restic', 'pkg_apt:ca-certificates'],
-        'unless': 'test -f /opt/packer/packer-{}'.format(PACKER_VER),
-    }
-}
 
 pkg_pip = {
     'mako': {},
@@ -27,15 +7,25 @@ pkg_pip = {
     },
 }
 
-actions = {
-    'install_packer': {
-        'command': 'cd /opt/packer && '
-                   'unzip -p packer_{0}.zip packer > packer-{0} && '
-                   'rm -f packer_{0}.zip &&'
-                   'chmod +x packer-{0} && '
-                   'ln -sf /opt/packer/packer-{0} /usr/local/sbin/packer'.format(PACKER_VER),
-        'unless': 'test -x /opt/packer/packer-{0} && '
-                  'cmp /usr/local/sbin/packer /opt/packer/packer-{0}'.format(PACKER_VER),
-        'needs': ['pkg_apt:unzip', 'download:/opt/packer/packer_{0}.zip'.format(PACKER_VER)]
+if node.has_bundle('apt'):
+    files['/etc/apt/sources.list.d/hashicorp.list'] = {
+        'content': 'deb [arch=amd64] https://apt.releases.hashicorp.com {release_name} main\n'.format(
+            release_name=node.metadata.get(node.os).get('release_name')
+        ),
+        'content_type': 'text',
+        'needs': ['file:/etc/apt/trusted.gpg.d/hashicorp.gpg', ],
+        'triggers': ["action:force_update_apt_cache", ],
     }
-}
+
+    files['/etc/apt/trusted.gpg.d/hashicorp.gpg'] = {
+        'content_type': 'binary',
+    }
+
+    pkg_apt = {
+        'packer': {
+            'needs': [
+                'file:/etc/apt/trusted.gpg.d/hashicorp.gpg',
+                'file:/etc/apt/sources.list.d/hashicorp.list',
+            ]
+        },
+    }
